30c2c2edf0899775807453ce6f77e820fd9daabe
s4a t4g <s4at4g@Mac.clear2>
2025-03-11 21:31:45 +0100

    Fix failing test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,16 +8,15 @@
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:galacticcoders_website/main.dart';
+import 'package:galacticcoders_website/screens/home_screen.dart';
+
 
 void main() {
-  testWidgets('Check if main text is there', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
+   testWidgets('App shows HomeScreen widget', (WidgetTester tester) async {
+    // Build the app.
     await tester.pumpWidget(const GalacticCoders());
 
-    // Verify that main text is exists.
-    expect(
-        find.text(
-            'Hello! We are Galactic Coders, a small but mighty group of individuals who share a passion for coding, collaboration, and learning. As a team, we enjoy working on various software development projects and ideas, organizing and participating in hackathons, and playing board games together. We are always looking to expand our knowledge and share it with others, and we are excited to see what the future holds for our group.'),
-        findsOneWidget);
+    // Check that HomeScreen is in the widget tree.
+    expect(find.byType(HomeScreen), findsOneWidget);
   });
 }
